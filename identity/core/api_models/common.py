1fac74c929125745b752ad556e79d26bdada1ac1
gosha20777 <gosha20777@live.ru>
2020-10-19 23:22:41 +0300

    feat: init ORM model

--- identity/core/api_models/common.py
+++ identity/core/api_models/common.py
@@ -1,4 +1,16 @@
 from pydantic import BaseModel
+from fastapi_utils.enums import StrEnum
+from enum import auto
 
 class Pong(BaseModel):
     pong: str = "Lacmus web API, version X.Y.Z"
+
+class RoleEnum(StrEnum):
+    root = 0
+    admin = 1
+    user = 2
+
+class TaskStatusEnum(StrEnum):
+    active = 0
+    finished = 1
+    failed = 2
--- identity/core/config.py
+++ identity/core/config.py
@@ -7,7 +7,13 @@ class WorkerConfig(BaseSettings):
     project_name: str = "Lacmus ml worker"
     api_prefix: str = "/api/v0"
     version: str = "0.1.0"
-    debug: bool = False  
+    debug: bool = False
+
+    db_user: str = "test"
+    db_password: str = "password"
+    db_host: str = "postgres"
+    db_port: int = 5432
+    db_name: str = "example"
 
     class Config:
         env_file = '.env'
