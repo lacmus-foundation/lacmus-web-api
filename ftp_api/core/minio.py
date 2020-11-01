d16d611d71894eb03e1a91662ad13d8c677fb03c
Ivan Povalyaev <ipovalyaev80@gmail.com>
2020-11-01 21:38:27 +0100

    replaced temp passwords with config params

--- ftp_api/core/minio.py
+++ ftp_api/core/minio.py
@@ -1,13 +1,14 @@
 from minio import Minio
 import os
+from commons import config
 
 from minio.error import (ResponseError, BucketAlreadyOwnedByYou,
                          BucketAlreadyExists)
 
 def create_project(project_name:str):
     minioClient = Minio('localhost:9000',
-                    access_key='qk1OtxQL54Cf',
-                    secret_key='8VE9zlm1Z0mb')
+                    access_key=config.MINIO_ACCESS_KEY,
+                    secret_key=config.MINIO_SECRET_KEY)
     try:
            minioClient.make_bucket(project_name)
     except BucketAlreadyOwnedByYou as err:
