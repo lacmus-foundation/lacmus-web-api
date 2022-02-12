122547eeefc4fbd483fa187c0110113413155b6e
Ivan Povalyaev <{ID}+{username}@users.noreply.github.com>
2022-02-12 12:17:29 +0100

    deployment scripts update

--- server_install/server_setup.sh
+++ server_install/server_setup.sh
@@ -3,6 +3,7 @@ chmod +x ./ansible.sh
 cp ansible_hosts /etc/ansible/hosts
 docker network create lacmus_server
 # you have to install postgres first, as other modules uses DB and will crush with it
+ansible-playbook playbooks/network.yml
 ansible-playbook playbooks/postgres_docker.yml
 ansible-playbook playbooks/minio_docker.yml
 ansible-playbook playbooks/ftp_docker.yml
