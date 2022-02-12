8e3ff6854a5d82800c9a73164760a3948500b449
Ivan Povalyaev <{ID}+{username}@users.noreply.github.com>
2022-02-12 13:03:44 +0100

    added ml worker with yolo5 scripts for Docker and ansible

--- server_install/server_setup.sh
+++ server_install/server_setup.sh
@@ -6,6 +6,7 @@ docker network create lacmus_server
 ansible-playbook playbooks/network.yml
 ansible-playbook playbooks/postgres_docker.yml
 ansible-playbook playbooks/minio_docker.yml
+ansible-playbook playbooks/ml_worker_yolo5.yml
 ansible-playbook playbooks/ftp_docker.yml
 ansible-playbook playbooks/identity_api_docker.yml
 ansible-playbook playbooks/orchestrator_docker.yml
